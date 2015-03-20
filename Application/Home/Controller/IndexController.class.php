0d7422c2376bcf79eea3258aefea11992406a8e3
lhzbxx <lhzbxx@gmail.com>
2015-03-20 16:46:23 +0800

    base

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -93,8 +93,23 @@ class IndexController extends Controller {
             //         $this->error("写入数据库失败~");
             //     }
             // }
+            $upload = new \Think\Upload();// 实例化上传类
+            $upload->maxSize = 3145728 ;// 设置附件上传大小
+            $upload->exts = array('jpg', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath = './Uploads/'; // 设置附件上传根目录
+            $upload->saveName = $fid;
+            $upload->autoSub = false;
+            $upload->savePath = ''; // 设置附件上传（子）目录
+            // 上传文件 
+            $info = $upload->upload();
+            if(!$info) { // 上传错误提示错误信息
+                $this->error($upload->getError());
+            } else { // 上传成功
+                $this->success('上传成功！');
+            }
+
             $Moneylist=M("List");
-            $add_arr['pic']="fixed";
+            $add_arr['pic']='./Uploads/'+$fid;
             $add_arr['amount']=$amount;
             $add_arr['info']=$info;
             $add_arr['date']=$date;
